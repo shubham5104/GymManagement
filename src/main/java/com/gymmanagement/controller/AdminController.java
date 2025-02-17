a3c1b04d0c250cd32d04ba215e3f7a63a97fd7f2
shubham5104 <shubhamkundalwal84@gmail.com>
2025-02-17 21:14:13 +0530

    admin controller

--- src/main/java/com/gymmanagement/controller/AdminController.java
+++ src/main/java/com/gymmanagement/controller/AdminController.java
@@ -1,5 +1,18 @@
 package com.gymmanagement.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
 public class AdminController {
+	
+	@GetMapping("/login")
+	public String login()
+	{
+		
+		return "Admin-Login";
+	}
 
 }
